671b0dd199392ac72b1bc5f6a0fac51141b28064
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-01-19 11:59:31 +0100

    NUEVI PROYECTO

---
+++ R7E01_FranGV_V2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace R7E01_FranGV_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
